e1cc8a6b6429eee6680f1cd55eeda83075e22cb5
KearneyJoe <44685213+KearneyJoe@users.noreply.github.com>
2023-01-18 17:58:20 -0500

    Updated stock performance stats + best time to buy and sell

--- src/app/stock-viz/stock-viz.component.ts
+++ src/app/stock-viz/stock-viz.component.ts
@@ -19,28 +19,44 @@ export class StockVizComponent implements OnInit {
 
   constructor(private stockService: StockVizService) { }
   stockData: StockModel[] = [];
-  ticker: string = 'SP500';
+  ticker: string = 'GOOGL';
+  avgHigh: number;
+  minHighData: StockModel | undefined;
+  maxHighData: StockModel | undefined;
+  bestBuyData: StockModel | undefined;
+  bestSellData: StockModel | undefined;
+  profit: number;
   stockForm: FormGroup;
+  statsCalculated: boolean = false;
   tickerValues: string[] = ['AMZN', 'APPL', 'CRM', 'GOOGL', 'META', 'MSFT', 'NFLX', 'SP500', 'TSLA'];
   private lineChart: Chart;
 
   ngOnInit(): void {
-    this.stockData = this.stockService.getData(this.ticker)
+    this.stockData = this.stockService.getData(this.ticker);
     this.stockForm = new FormGroup({
       ticker: new FormControl(this.ticker)
     });
     this.createLineChart(this.stockData);
+    let stockStats = this.stockService.calculateStockStats(this.stockData);
+    this.avgHigh = stockStats.avgHigh;
+    this.minHighData = stockStats.minHighData;
+    this.maxHighData = stockStats.maxHighData;
+    this.bestBuyData = stockStats.bestBuyData;
+    this.bestSellData = stockStats.bestSellData;
+    this.profit = stockStats.profit;
+    this.statsCalculated = true;
   }
 
   onSubmit() {
-    this.ticker = this.stockForm.value.ticker
-    this.stockData = this.stockService.getData(this.ticker)
+    this.ticker = this.stockForm.value.ticker;
+    this.stockData = this.stockService.getData(this.ticker);
     this.createLineChart(this.stockData, true);
+    this.stockService.bestBuyAndSell(this.stockData);
   }
 
   createLineChart(stockData: StockModel[], destroy: boolean = false) {
     if (destroy) {
-      this.lineChart.destroy()
+      this.lineChart.destroy();
     }
     // extract the dates and high values from the stock data
     const dates = stockData.map(data => data.Date);
@@ -78,76 +94,5 @@ export class StockVizComponent implements OnInit {
 
   }
 
-
-
-
-
-  // private createSvg(): void {
-  //   // set the dimensions and margins of the graph
-  //   var margin = { top: 10, right: 30, bottom: 30, left: 60 },
-  //     width = 460 - margin.left - margin.right,
-  //     height = 400 - margin.top - margin.bottom;
-
-  //   // append the svg object to the body of the page
-  //   this.svg = d3.select("#my_dataviz")
-  //     .append("svg")
-  //     .attr("width", width + margin.left + margin.right)
-  //     .attr("height", height + margin.top + margin.bottom)
-  //     .append("g")
-  //     .attr("transform",
-  //       "translate(" + margin.left + "," + margin.top + ")");
-  // }
-
-
-  // private drawplot(data: StockModel[]): void {
-  //   this.createSvg();
-
-  //   interface DataPoint {
-  //     date: Date;
-  //     high: number;
-  //   }
-
-  //   let plotData: DataPoint[] = []
-  //   let maxHigh = 0;
-  //   for (let i of data) {
-  //     let date = new Date(i.Date);
-  //     if (date) {
-  //       plotData.push({
-  //         date: date,
-  //         high: i.High
-  //       });
-  //     }
-  //     if (i.High > maxHigh) {
-  //       maxHigh = i.High
-  //     }
-  //   }
-
-  //   var xScale = d3.scaleLinear()
-  //     .domain([2018, 2022])
-  //     .range([0, this.width]);
-
-  //   this.svg.append("g")
-  //     .attr("transform", "translate(0," + this.height + ")")
-  //     .call(d3.axisBottom(xScale).tickFormat(d3.format("d")));
-
-
-  //   var yScale = d3.scaleLinear()
-  //     .domain([0, maxHigh])
-  //     .range([this.height, 0]);
-
-  //   this.svg.append("g")
-  //     .call(d3.axisLeft(yScale));
-
-  //   // Add the line
-  //   this.svg.append("path")
-  //     .datum(plotData)
-  //     .attr("fill", "none")
-  //     .attr("stroke", "steelblue")
-  //     .attr("stroke-width", 1.5)
-  //     .attr("d", d3.line()
-  //       .x(function (d) { return xScale(d.date) })
-  //       .y(function (d) { return yScale(d.high) })
-  //     )
-  // }
 
 }
--- src/app/stock-viz/stock-viz.service.ts
+++ src/app/stock-viz/stock-viz.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { map } from "rxjs";
+import { max, min } from "moment";
 // import { environment } from "../environments/environment";
 import { StockModel } from "../models/stock-model";
 import { stockData } from "./stock_data_updated";
@@ -30,6 +30,68 @@ export class StockVizService {
             }
         }
         return filteredData
+    }
+
+    bestBuyAndSell(stockData: StockModel[]) {
+
+        let buy = 0;
+        let bestBuyIdx = 0;
+        let sell = 1;
+        let bestSellIdx = 1;
+        let profit = 0;
+
+        while (sell < stockData.length) {
+            if (stockData[buy].High >= stockData[sell].High) {
+                buy = sell
+                sell = sell + 1
+            } else {
+                if (stockData[sell].High - stockData[buy].High > profit) {
+                    profit = stockData[sell].High - stockData[buy].High
+                    bestBuyIdx = buy
+                    bestSellIdx = sell
+                }
+                sell += 1
+            }
+        }
+        return {
+            profit: profit,
+            bestBuyIdx: bestBuyIdx,
+            bestSellIdx: bestSellIdx,
+        }
+    }
+
+    calculateStockStats(stockData: StockModel[]) {
+
+        let maxHigh = 0;
+        let maxHighData: StockModel | undefined;
+        let minHigh = null;
+        let minHighData: StockModel | undefined;
+
+        let avgHigh = stockData.reduce((a, b) => a + b.High, 0) / stockData.length
+        for (let day of stockData) {
+
+            //Find Max
+            if (day.High > maxHigh) {
+                maxHigh = day.High
+                maxHighData = day
+            }
+
+            //Find min
+            if (minHigh === null || day.High < minHigh) {
+                minHigh = day.High
+                minHighData = day
+            }
+        }
+        let bestDaysData = this.bestBuyAndSell(stockData)
+
+        return {
+            avgHigh: avgHigh,
+            minHighData: minHighData,
+            maxHighData: maxHighData,
+            bestBuyData: stockData[bestDaysData.bestBuyIdx],
+            bestSellData: stockData[bestDaysData.bestSellIdx],
+            profit: bestDaysData.profit
+        }
     }
 
 }
